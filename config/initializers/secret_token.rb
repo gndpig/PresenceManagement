da81b303e166b77d5037195976d53dae44add018
Taiki Nakatsuka <gndpig@gndpig.local>
2012-10-29 10:41:35 +0900

    first initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PresenceManagement::Application.config.secret_token = 'c56800feb6ed2ac829b04cc3e0bde4d4524b53cb0c13574fecbb497325428a51e4a40790e64fb10d330d60b480c116a48c0900db61014dca0e75b5eb8dc70900'
